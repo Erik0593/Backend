e2d747d6a1d59518b950e8b11e3cf49d3ea4d79c
Erik Gutierrez <gutierrez_m_e@hotmail.com>
2022-01-21 21:14:47 -0600

    comenatrios

--- saludo.js
+++ saludo.js
@@ -1,8 +1,13 @@
+//archivo de otro dev para separar --name=nombre y solo sacar el nombre.
 const params = require('yargs-parser')(process.argv.slice(2))
+//achivo de los colores para las letras
 const colors = require('colors')
 
+
+//aqui ya sacamos el nombre de params
 const nombre = params.name
 
+//saludamos al wey que esta ingresando a la computadora
 const saludo = (nombre) => {
     if(typeof nombre === 'string'){
         console.log(`Bienvenido ${nombre}`.rainbow)
